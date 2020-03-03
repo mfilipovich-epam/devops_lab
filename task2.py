48c254e1670ddd44c3e1fd88364e7fe6ad1cf03a
Maxim Filipovich <maxim_filipovich@epam.com>
2020-03-03 21:13:39 +0300

    task 2 corretion

--- task2.py
+++ task2.py
@@ -5,10 +5,14 @@ l2 = input().split()
 
 
 def compare_list(l1, l2):
+    for i in range(len(l1)):
+        l1[i] = int(l1[i])
+    for i in range(len(l2)):
+        l2[i] = int(l2[i])
     if len(l1) != len(l2):
         diff = len(l1) - len(l2)
         for i in range(diff):
-            l2.append('None')
+            l2.append(None)
     return dict(zip(l1, l2))
 
 
