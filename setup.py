29e12ec31b903a611feae8489298d3e25576abc9
Maxim Filipovich <maxim_filipovich@epam.com>
2020-03-04 21:14:54 +0300

    day4 initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="pr-stats",
+    packages=setuptools.find_packages(),
+    scripts=['pr-stats'],
+    version="0.14",
+    author="Maxim Filipovich",
+    author_email="maxim_filipovich@epam.com",
+    description="Day 4 hometask"
+)
