ef1c0a09579c46b74bf006c118d70a090c17c0af
Maxim Filipovich <maxim_filipovich@epam.com>
2020-03-04 10:36:41 +0300

    task2 and task3 correction

--- task2.py
+++ task2.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 
-l1 = input().split()
-l2 = input().split()
-
+l1 = list(map(int, input().split()))
+l2 = list(map(int, input().split()))
 
 def compare_list(l1, l2):
-    for i in range(len(l1)):
-        l1[i] = int(l1[i])
-    for i in range(len(l2)):
-        l2[i] = int(l2[i])
     if len(l1) != len(l2):
         diff = len(l1) - len(l2)
         for i in range(diff):
--- task3.py
+++ task3.py
@@ -7,4 +7,3 @@ n = int(input())
 for i in range(1, n + 1):
     fact *= i
 print(fact)
-
