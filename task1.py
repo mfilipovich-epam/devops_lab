9e4af2ffcf1516e87fea22bbd501b2277046f48f
Maxim Filipovich <maxim_filipovich@epam.com>
2020-03-05 16:17:20 +0300

    task1.py correction

--- task1.py
+++ task1.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 sum = 0
-count = 0
 s_list = {}
 
 x = input()
@@ -10,10 +9,10 @@ for i in range(int(x)):
     inp_st = input().split()
     name = inp_st[0]
     marks = inp_st[1:]
+    sum = 0
     for numbers in marks:
         sum += float(numbers)
-        count += 1
-    s_list[name] = sum / count
+    s_list[name] = sum / 3
 
 user = input()
 print("{:.2f}".format(s_list.get(user)))
